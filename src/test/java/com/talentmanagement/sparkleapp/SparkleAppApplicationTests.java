7458fdf7a926fab8ac2b34e9af180c8fac79a3f4
David-Bamgbade <davidbamgbade082@gmail.com>
2024-09-26 12:27:19 +0100

    first push

---
+++ src/test/java/com/talentmanagement/sparkleapp/SparkleAppApplicationTests.java
@@ -0,0 +1,13 @@
+package com.talentmanagement.sparkleapp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SparkleAppApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
